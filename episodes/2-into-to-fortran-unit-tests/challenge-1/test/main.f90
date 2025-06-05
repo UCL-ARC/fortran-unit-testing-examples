d7f5e61e9c0603543f0c47722acd347c736c03f3
Connor Aird <c.aird@ucl.ac.uk>
2025-06-05 16:25:29 +0100

    Add test for transpose_matrix

---
+++ episodes/2-into-to-fortran-unit-tests/challenge-1/test/main.f90
@@ -0,0 +1,23 @@
+program test_main
+    use veggies, only : test_item_t, test_that, run_tests
+
+    ! If you wish to run the provided solution replace maths_test with maths_test_solution in the line below
+    use transpose_matrix_test, only : transpose_matrix_test_suite
+    implicit none
+
+    if (.not.run()) stop 1
+
+contains
+    function run() result(passed)
+        logical :: passed
+
+        type(test_item_t) :: tests
+        type(test_item_t) :: individual_tests(1)
+
+        individual_tests(1) = transpose_matrix_test_suite()
+
+        tests = test_that(individual_tests)
+
+        passed = run_tests(tests)
+    end function run
+end program test_main
---
+++ episodes/2-into-to-fortran-unit-tests/challenge-1/test/transpose_matrix_test.f90
@@ -0,0 +1,151 @@
+module transpose_matrix_test
+    use cli, only : cli_args_t
+    use matrix_operations, only : transpose_matrix
+    use veggies, only: &
+            assert_equals, &
+            describe, &
+            example_t, &
+            fail, &
+            input_t, &
+            it, &
+            result_t, &
+            test_item_t
+
+    implicit none
+
+    private
+    public :: transpose_matrix_test_suite
+
+    !! @class test_parameters_t
+    !!
+    !! @brief A class to store test parameters
+    type, extends(input_t) :: test_parameters_t
+        integer :: nx, ny
+        real, dimension(:,:), allocatable :: matrix, expected_result
+    end type test_parameters_t
+
+    interface test_parameters_t
+        module procedure test_parameters_constructor
+    end interface test_parameters_t
+
+contains
+
+    !> A constructor for the test_parameters_t type
+    pure function test_parameters_constructor(nx, ny, matrix, expected_result) result(test_parameters)
+        integer, intent(in) :: nx, ny
+        real, dimension(:,:), allocatable, intent(in) :: matrix
+        real, dimension(:,:), allocatable, intent(in) :: expected_result
+
+        type(test_parameters_t) :: test_parameters
+        integer :: x, y
+
+        test_parameters%nx = nx
+        test_parameters%ny = ny
+
+        allocate(test_parameters%matrix(ny, nx))
+        allocate(test_parameters%expected_result(nx, ny))
+        do y = 1, ny
+            do x = 1, nx
+                test_parameters%matrix(y, x) = matrix(y, x)
+                test_parameters%expected_result(x, y) = expected_result(x, y)
+            end do
+        end do
+    end function test_parameters_constructor
+
+    !> The test suite for the matrix_operations module tests
+    function transpose_matrix_test_suite() result(tests)
+        type(test_item_t) :: tests
+
+        real, dimension(:,:), allocatable :: input_matrix, output_matrix
+        type(example_t), dimension(5) :: transpose_matrix_test_data
+
+        ! 3x3 Matrices tests
+        allocate(input_matrix(3,3))
+        allocate(output_matrix(3,3))
+
+        ! Identity
+        input_matrix(:,1) = [1.0,0.0,0.0]
+        input_matrix(:,2) = [0.0,1.0,0.0]
+        input_matrix(:,3) = [0.0,0.0,1.0]
+        transpose_matrix_test_data(1) = example_t(test_parameters_t(3, 3, input_matrix, input_matrix))
+
+        ! Zero Matrix
+        input_matrix(:,1) = [0.0,0.0,0.0]
+        input_matrix(:,2) = [0.0,0.0,0.0]
+        input_matrix(:,3) = [0.0,0.0,0.0]
+        transpose_matrix_test_data(2) = example_t(test_parameters_t(3, 3, input_matrix, input_matrix))
+
+        ! Unsymetric matrix
+        input_matrix(:,1) = [1.0,2.0,3.0]
+        input_matrix(:,2) = [4.0,5.0,6.0]
+        input_matrix(:,3) = [7.0,8.0,9.0]
+
+        output_matrix(:,1) = [1.0,4.0,7.0]
+        output_matrix(:,2) = [2.0,5.0,8.0]
+        output_matrix(:,3) = [3.0,6.0,9.0]
+        transpose_matrix_test_data(3) = example_t(test_parameters_t(3, 3, input_matrix, output_matrix))
+
+        deallocate(input_matrix)
+        deallocate(output_matrix)
+
+        ! 2x3 Matrices tests
+        allocate(input_matrix(3,2))
+        allocate(output_matrix(2,3))
+
+        ! Zero Matrix
+        input_matrix(:,1) = [0.0,0.0,0.0]
+        input_matrix(:,2) = [0.0,0.0,0.0]
+
+        output_matrix(:,1) = [0.0, 0.0]
+        output_matrix(:,2) = [0.0, 0.0]
+        output_matrix(:,3) = [0.0, 0.0]
+        transpose_matrix_test_data(4) = example_t(test_parameters_t(2, 3, input_matrix, output_matrix))
+
+        ! Unsymetric matrix
+        input_matrix(:,1) = [1.0,2.0,3.0]
+        input_matrix(:,2) = [4.0,5.0,6.0]
+
+        output_matrix(:,1) = [1.0,4.0]
+        output_matrix(:,2) = [2.0,5.0]
+        output_matrix(:,3) = [3.0,6.0]
+        transpose_matrix_test_data(5) = example_t(test_parameters_t(2, 3, input_matrix, output_matrix))
+
+        deallocate(input_matrix)
+        deallocate(output_matrix)
+
+        tests = describe( &
+                    "matrix_operations::transpose_matrix", &
+                    [ it( &
+                        "works as expected", &
+                        transpose_matrix_test_data, &
+                        test_transpose_matrix) &
+                    ])
+    end function transpose_matrix_test_suite
+
+    !> A unit test for the matrix_operations::transpose_matrix subroutine.
+    !!
+    !! @param inputs - An instance of the test_parameters_t containing function
+    !!                 inputs and expected outputs.
+    !!
+    !! @returns result_ - The result of the test (pass or fail) of type result_t
+    function test_transpose_matrix(input) result(result_)
+        class(input_t), intent(in) :: input
+        type(result_t) :: result_
+
+        real, dimension(:,:), allocatable :: actual_result
+
+        select type (input)
+        type is (test_parameters_t)
+            allocate(actual_result(input%nx, input%ny))
+
+            call transpose_matrix(input%matrix, actual_result)
+
+            result_ = assert_equals(input%expected_result, actual_result)
+
+            deallocate(actual_result)
+        class default
+            result_ = fail("Didn't get test_parameters_t")
+        end select
+
+    end function test_transpose_matrix
+end module transpose_matrix_test
